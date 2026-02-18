79d00eaad1436b2c582f31b2c025e9da49033102
Senny254 <senny1328@gmail.com>
2026-02-18 16:54:05 +0300

    created and styled the header section

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import Header from "../src/Header/Header"
+import Hero from "../src/Hero/Hero"
+import About from './About/About'
+import Skills from './Skills/Skills'
+import Projects from './Projectss/Projects'
+import Exp from './Experience/Exp'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      
+    <Header/>  
+    <Hero/> 
+    <About/> 
+    <Exp/>
+    <Projects/>
+    <Skills/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Header/Header.jsx
@@ -0,0 +1,27 @@
+import "./Header.css"
+function Header ()
+    {
+        return (
+             <>
+             <div className="header">
+                <div className="lefttitle">
+                    <h2>Ruth Mutisya</h2>
+                </div>
+                <div className="right">
+                    <nav className="nav-section">
+                        <li> <a href="about"></a>About</li>
+                         <li> <a href=""></a>Skills</li>
+                          <li><a href=""></a>projects</li>
+                           <li><a href=""></a>experience</li>
+                            <li><a href=""></a>contact</li>
+                    </nav>
+                    
+                </div>
+             </div>
+
+        </>
+
+        )
+       
+    }
+    export default Header
